26d6c25b6dd51f04395dc17c42eca406502fe009
[dyad] <marcelo.net77@hotmail.com>
2026-02-25 15:04:47 +0000

    [dyad] Fixing useEditablePlaylists export error - wrote 1 file(s)

--- src/features/playlists/index.ts
+++ src/features/playlists/index.ts
@@ -1,5 +1,5 @@
 export type { Playlist, PlaylistVideo, PlaylistProgress, PlaylistCollaborator } from '@/entities/playlist/playlist.types';
-export { usePlaylists, usePlaylistById, useCreatePlaylist, useUpdatePlaylist, useDeletePlaylist } from './queries/usePlaylists';
+export { usePlaylists, useEditablePlaylists, usePlaylistById, useCreatePlaylist, useUpdatePlaylist, useDeletePlaylist } from './queries/usePlaylists';
 export { usePlaylistVideos, useAddVideoToPlaylist, useRemoveVideoFromPlaylist, useReorderPlaylistVideos } from './queries/usePlaylistVideos';
 export { usePlaylistCollaborators, useAddCollaborator, useUpdateCollaboratorRole, useRemoveCollaborator } from './queries/usePlaylistCollaborators';
 export { usePlaylistProgress, useMarkVideoWatched } from './queries/usePlaylistProgress';
