e13f9ad6897a84632c03175625d101a4c862ef19
Marcelo Santos <marcelo.net@hotmail.com>
2026-06-09 22:54:54 +0100

    chore: docs

--- supabase/migrations/20260513161000_grant_list_featured_videos_execute_to_client_roles.sql
+++
@@ -1,4 +0,0 @@
--- Restore client access to homepage featured videos RPC.
--- This function is SECURITY DEFINER and safe to expose with EXECUTE.
-
-grant execute on function public.list_featured_videos(integer, integer) to anon, authenticated;
